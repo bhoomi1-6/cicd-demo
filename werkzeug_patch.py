73e774d3b0bbea2cb955656cf513dd5c5cd5ba3d
Bhoomi Bhat <92518502+bhoomi1-6@users.noreply.github.com>
2024-11-20 12:51:45 +0530

    Update werkzeug_patch.py

--- werkzeug_patch.py
+++ werkzeug_patch.py
@@ -6,3 +6,6 @@ from urllib.parse import urlparse as url_parse
 import werkzeug.urls
 werkzeug.urls.url_quote = url_quote
 werkzeug.urls.url_parse = url_parse
+# Add the `__version__` attribute if missing
+if not hasattr(werkzeug, "__version__"):
+    werkzeug.__version__ = "2.0.3"  # Set to a compatible version
