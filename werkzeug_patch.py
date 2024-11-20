b092c7be6b01e1035bd91d3c3da570949c2757d8
Bhoomi Bhat <92518502+bhoomi1-6@users.noreply.github.com>
2024-11-20 12:49:46 +0530

    Update werkzeug_patch.py

--- werkzeug_patch.py
+++ werkzeug_patch.py
@@ -1,6 +1,8 @@
 # werkzeug_patch.py
 from urllib.parse import quote as url_quote
+from urllib.parse import urlparse as url_parse
 
 # Apply the patch
 import werkzeug.urls
 werkzeug.urls.url_quote = url_quote
+werkzeug.urls.url_parse = url_parse
